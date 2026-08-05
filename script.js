1746949e6b3d3a6310e34f547e2a33828374faf9
soundhar0068-art <soundhar0068@gmail.com>
2026-08-05 15:51:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("loginForm").addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    let user = document.getElementById("user").value;
+    let pass = document.getElementById("pass").value;
+    let msg = document.getElementById("msg");
+
+    if(user === "admin" && pass === "1234"){
+
+        msg.innerHTML = "Login Successful ✅";
+        msg.style.color = "green";
+
+    }else{
+
+        msg.innerHTML = "Invalid Username or Password ❌";
+        msg.style.color = "red";
+    }
+
+});
